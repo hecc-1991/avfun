f50788b884a2a579af762fad0cb4412c0d29f5ec
18158 <18158536350@163.com>
2021-01-24 02:08:32 +0800

    optimize log util

--- codec/LogUtil.h
+++ codec/LogUtil.h
@@ -6,10 +6,10 @@ namespace avfun
 
 #ifdef _WIN32
 
-#define LOG_INFO(format,...) fprintf(stdout, "##avfun## %s:%d(%s)>>[info]::"#format"\n",__FILE__,__LINE__,__func__,##__VA_ARGS__)
-#define LOG_DEBUG(format,...) fprintf(stdout, "##avfun## %s:%d(%s)>>[debug]::"#format"\n",__FILE__,__LINE__,__func__,##__VA_ARGS__)
-#define LOG_WARNING(format,...) fprintf(stdout, "##avfun## %s:%d(%s)>>[warning]::"#format"\n", __FILE__,__LINE__,__func__,##__VA_ARGS__)
-#define LOG_ERROR(format,...) fprintf(stdout, "##avfun## %s:%d(%s)>>[error]::"#format"\n", __FILE__,__LINE__,__func__,##__VA_ARGS__)
+#define LOG_INFO(format,...) fprintf(stdout, "[info]#avfun# %s:%d(%s)::"#format"\n",__FILE__,__LINE__,__func__,##__VA_ARGS__)
+#define LOG_DEBUG(format,...) fprintf(stdout, "[debug]#avfun# %s:%d(%s)::"#format"\n",__FILE__,__LINE__,__func__,##__VA_ARGS__)
+#define LOG_WARNING(format,...) fprintf(stdout, "[warning]#avfun# %s:%d(%s)>>::"#format"\n", __FILE__,__LINE__,__func__,##__VA_ARGS__)
+#define LOG_ERROR(format,...) fprintf(stdout, "[error]#avfun# %s:%d(%s)::"#format"\n", __FILE__,__LINE__,__func__,##__VA_ARGS__)
 
 #elif __ANDROID__
 
